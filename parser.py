3e3831c7bcd3f14b4bb4d02e829211b25089be5c
Denis123-commit1 <financialstudent1@gmail.com>
2021-09-07 13:19:02 +0300

    6 commit
    issues:
    
    1. Solved problem with link

--- parser.py
+++ parser.py
@@ -14,28 +14,23 @@ def get_html(url, params=None):
 
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
-    items = soup.find_all('a', class_='proposition_link')
+    items = soup.find_all('div', class_='result-explore')
 
     # print(items)
 
     cars = []
     for item in items:
-        # uah_price = item.find('span', class_='size15')
-        # if uah_price:
-        #     uah_price = uah_price.get_text().replace(' • ', '')
-        # else:
-        #     uah_price = 'Цену уточняйте'
+
 
         cars.append({
             'title': item.find('div', class_='proposition_title').get_text(strip=True),
-            # 'link': item.find('a',class_ = 'proposition_link').get('href'),
+            'link': item.find('a',class_ = 'proposition_link').get('href'),
             'usd_price': item.find('span', class_='green').get_text(),
             'uah_price': item.find('span', class_='size16').get_text(),
             'city': item.find('span', class_='item region').get_text(),
         })
-    print(cars) # Проверка количества названий автомобилей
-    # print(len(cars))
-    # return cars
+    print(cars)
+    return cars
 
 
 
@@ -47,28 +42,10 @@ def parse():
     html = get_html(URL)
     if html.status_code == 200:
         # print(html.text) # Выйдет html запрос, который надо будет распарсить
-        get_content(html.text)
-        # cars = get_content(html.text)
+        cars = get_content(html.text)
     else:
         print('Error')
 
 
 parse()
 
-
-
-
-
-
-# import requests
-# from bs4 import BeautifulSoup as BS
-#
-# r = requests.get('https://auto.ria.com/newauto/marka-jeep/')
-# html = BS(r.content, 'html.parser')
-#
-#
-#
-#
-# for el in html.select('.anons-readmore'):
-#                 url_more = el.select_one('a').get('href')
-#                 print(url_more)
