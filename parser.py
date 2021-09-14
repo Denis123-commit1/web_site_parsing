3ed3a2c27b4e364386ad822561735642f31a4615
Denis123-commit1 <financialstudent1@gmail.com>
2021-09-14 14:57:08 +0300

    Добавили дату парсинга

--- parser.py
+++ parser.py
@@ -53,12 +53,16 @@ def get_ip(html):
             item_art.group(0)
         item_href = 'https://leroymerlin.ru' + item.find_next('a').get('href')
         item_name = re.search(item.text[13:80], item.text)
-        # print(item_name.start())
-        # if item_name:
-        #     item_name.group(0)
-        all_categories_dict_rad_stal[item_art.group(0)] = item_name.group(0), item_href
+        item_data = datetime.now()
+        # item_price = re.search('\d{4}\s₽/шт', item.text)
+        # print(item_price.start())
+        # item_price = re.search('\d{3}\s₽/шт', item.text)
+        # if item_price:
+        #     item_price.group(0).replace(" ","")
+
+        all_categories_dict_rad_stal[item_art.group(0)] = item_name.group(0), item_href, item_data
     with open("all_categories_dict_rad_stal_attempt_art_name_link.json", "w", encoding='utf8') as file:
-        json.dump(all_categories_dict_rad_stal, file, indent=4, ensure_ascii=False)
+        json.dump(all_categories_dict_rad_stal, file, indent=4, ensure_ascii=False, sort_keys=True, default=str)
     print('-----------')
 
 def main():
