c3b2622e9f7d6f844b5fd1953c85671cd817061d
Denis123-commit1 <financialstudent1@gmail.com>
2021-09-16 14:17:30 +0300

    Не работает, но чистенькая функция

--- parser.py
+++ parser.py
@@ -17,9 +17,9 @@ import re
 from random import choice
 from random import uniform
 from datetime import datetime
+import os
 
-
-
+FILE = r'C:\Users\Dell\PycharmProjects\web_site_parsing\radiators.csv'
 
 # Настраиваем несколько прокси
 def get_html(url, useragent = None, proxy = None):
@@ -47,61 +47,52 @@ def get_ip(html):
     for item in all_products_hrefs:
         item_text = item.text
         item_href_1 = 'https://leroymerlin.ru/' + item.get("href")
-        # print(item_href_1)
-        all_categories_dict_for_radiators_and_elther[item_text] = item_href_1
 
-    # with open("all_categories_dict_for_radiators_and_elther.json", "w", encoding='utf8') as file:
-    #     json.dump(all_categories_dict_for_radiators_and_elther, file, indent=4, ensure_ascii=False)
-    # print('---'*20)
+    radiators = []
+    for item_href_1 in all_categories_dict_for_radiators_and_elther:
 
-    iteration_count = int(len(all_categories_dict_for_radiators_and_elther)) - 1
-    count = 0
-    print(f"Всего итераций: {iteration_count}")
+        print('New proxy & New UserAgent:')
+        soup = BeautifulSoup(item_href_1, 'lxml')
+        all_products_hrefs = soup.find_all("div", class_ = "phytpj4_plp")
+        all_categories_dict_rad_stal = {}
+        for item in all_products_hrefs:
+            item_art = re.search('\d{8}', item.text)
+            if item_art:
+                item_art.group(0)
+            item_href = 'https://leroymerlin.ru' + item.find_next('a').get('href')
+            item_name = re.search(item.text[13:80], item.text)
+            item_data = datetime.now()
+            item_price = re.search('\d{1}\s\d{3}\s₽/шт|\d{2}\s\d{3}\s₽/шт', item.text)
+            for_adding = all_categories_dict_rad_stal[item_art.group(0)] = item_name.group(0), item_href, item_data, item_price.group(0).replace(" ","")
+            radiators.append({
+                'link' : item_href,
+                'name' : item_name,
+                'date' : item_data,
+                'price': item_price,
+                'art': item_art
+            })
+    return radiators
 
-
-    # for item_href_1 in all_categories_dict_for_radiators_and_elther:
-    #
-    #     print('New proxy & New UserAgent:')
-    #     soup = BeautifulSoup(item_href_1, 'lxml')
-    #     all_products_hrefs = soup.find_all("div", class_ = "phytpj4_plp")
-    #     all_categories_dict_rad_stal = {}
-    #     for item in all_products_hrefs:
-    #         item_art = re.search('\d{8}', item.text)
-    #         if item_art:
-    #             item_art.group(0)
-    #         item_href = 'https://leroymerlin.ru' + item.find_next('a').get('href')
-    #         item_name = re.search(item.text[13:80], item.text)
-    #         item_data = datetime.now()
-    #         item_price = re.search('\d{1}\s\d{3}\s₽/шт|\d{2}\s\d{3}\s₽/шт', item.text)
-    #         for_adding = all_categories_dict_rad_stal[item_art.group(0)] = item_name.group(0), item_href, item_data, item_price.group(0).replace(" ","")
-    #
-    #         with open(f"data/{item_text}.csv", "w", encoding="utf-8") as file:
-    #             writer = csv.writer(file)
-    #             writer.writerow(
-    #                 (
-    #                     item_href,
-    #                     item_art.group(0),
-    #                     item_data,
-    #                     item_price.group(0).replace(" ",""),
-    #                     item_name.group(0)
-    #                 )
-    #             )
+def save_file(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['ссылка', 'имя', 'дата', 'Цена в руб', 'артикул'])
+        for item in items:
+            writer.writerow([item['link'], item['name'], item['date'], item['price'], item['art']])
 
 
 
 
 
 def main():
-    # with open("all_categories_dict_for_radiators_and_elther.json", encoding="utf8") as file:
-    #     all_categories = json.load(file)
-    #
-    # for item_text ,item_href_1 in all_categories.items():
-    #     if len(item_text) > 20:
-    #         url = f'{item_href_1}'
-        url = 'https://leroymerlin.ru/catalogue/radiatory-otopleniya/'
+    with open("all_categories_dict_for_radiators_and_elther.json", encoding="utf8") as file:
+        all_categories = json.load(file)
 
+    for item_text ,item_href_1 in all_categories.items():
+        if len(item_text) > 20:
+            url = f'{item_href_1}'
+        # url = 'https://leroymerlin.ru/catalogue/radiatory-otopleniya/'
 
-        # print(url)
         useragents = open('useragents.txt').read().split('\n')
         proxies = open('proxies.txt').read().split('\n')
         # Создаем видимость что парсит человек и рандомизируем прокси и юзер агент
@@ -111,15 +102,26 @@ def main():
             useragent = {'User-Agent': choice(useragents)}
             try:
                 html = get_html(url, useragent, proxy)
+                pages_count = get_pages_count(html)
+                if html.status_code == 200:
+                    radiators = []
+                    pages_count = get_pages_count(html.text)
+
+                    for page in range(1, pages_count + 1):
+                        print(f'Парсинг страницы {page} из {pages_count}...')
+                        html = get_html(url, params={'page': page})
+                        radiators.extend(get_ip(html.text))
+                    save_file(radiators, FILE)
+                    print(f'Получено {len(radiators)} автомобилей')
+                    os.startfile(FILE)
+                else:
+                    print('Error')
             except:
                 continue
             try:
                 get_ip(html)
             except AttributeError:
                 continue
-            pages_count = get_pages_count(html)
-            print(pages_count)
-
 
 
 
