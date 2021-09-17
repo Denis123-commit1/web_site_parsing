4e83d59e417f6009dfead0129a942ab34cc79d4c
Denis123-commit1 <financialstudent1@gmail.com>
2021-09-17 14:18:45 +0300

    <re.Match object; span=(3, 4), match='8'> в Дебагере вышло

--- parser.py
+++ parser.py
@@ -30,8 +30,11 @@ def get_html(url, useragent = None, proxy = None, params = None):
 def get_pages_count(html):
     soup = BeautifulSoup(html, "lxml")
     pagination = soup.find_all("div", class_ = "f11n7m8x_plp")
+
     if pagination:
-        return int(re.search('\d{1}|\d{2}', pagination[-1].get_text()).group(0))
+        pagination_1 = print(re.search('\d', (pagination[-1].get_text())))
+        return int(pagination_1[0])
+
     else:
         return 1
 
@@ -47,12 +50,6 @@ def get_ip(html):
 
     radiators = []
     for item in items:
-
-        # print('New proxy & New UserAgent:')
-        # soup = BeautifulSoup(item_href_1, 'lxml')
-        # all_products_hrefs = soup.find_all("div", class_ = "phytpj4_plp")
-        # all_categories_dict_rad_stal = {}
-        # for item in all_products_hrefs:
         item_art = re.search('\d{8}', item.text)
         print(item_art)
 
@@ -72,112 +69,53 @@ def get_ip(html):
         })
     return radiators
 
+def save_json(name):
+    with open(f"{name}", "w", encoding='utf8') as file:
+        json.dump({name}, file, indent=4, ensure_ascii=False)
+
+
+def load_json(name):
+    with open(f"{name}", encoding="utf8") as file:
+        name = json.load(file)
+
+
 def save_file(items, path):
     with open(path, 'w', newline='') as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerow(['ссылка', 'имя', 'дата', 'Цена в руб', 'артикул'])
         for item in items:
-            writer.writerow([item['link'], item['name'].group(0), item['date'], item['price'].group(0).replace(" ",""), item['art'].group(0)])
+            writer.writerow([item['link'], item['name'], item['date'], item['price'], item['art']])
 
 
 def parse():
-    url = 'https://leroymerlin.ru/catalogue/radiatory-otopleniya/'
-    html = get_html(url)
+    url = "https://leroymerlin.ru/catalogue/radiatory-bimetallicheskie/"
     useragents = open('useragents.txt').read().split('\n')
     proxies = open('proxies.txt').read().split('\n')
     # Создаем видимость что парсит человек и рандомизируем прокси и юзер агент
-    sleep(uniform(3, 12))
-    proxy = {'http': 'http://' + choice(proxies)}
-    useragent = {'User-Agent': choice(useragents)}
-    html = get_html(url, useragent, proxy)
-    try:
+    for i in range(4):
+        sleep(uniform(3, 12))
+        proxy = {'http': 'http://' + choice(proxies)}
+        useragent = {'User-Agent': choice(useragents)}
+
+        html = get_html(url, useragent, proxy)
         radiators = []
-        pages_count = get_pages_count(html)
+        pages_count = int(get_pages_count(html))
+        print(pages_count)
         for page in range(1, pages_count + 1):
             print(f'Парсинг страницы {page} из {pages_count}...')
             html = get_html(url, params={'page': page})
-            radiators.extend(get_ip(html))
+            radiators.extend(get_ip(html.text))
         save_file(radiators, FILE)
-        print(f'Получено {len(radiators)} автомобилей')
+        print(f'Получено {len(radiators)} материалов')
         os.startfile(FILE)
-    finally: print('ok')
-    try:
-        get_ip(html)
-    except AttributeError:
-        print('ok')
+
 
 
 def main():
-    # useragents = open('useragents.txt').read().split('\n')
-    # proxies = open('proxies.txt').read().split('\n')
-    # # Создаем видимость что парсит человек и рандомизируем прокси и юзер агент
-    # for i in range(2):
-    #     sleep(uniform(3, 12))
-    #     proxy = {'http': 'http://' + choice(proxies)}
-    #     useragent = {'User-Agent': choice(useragents)}
-    # # with open("all_categories_dict_for_radiators_and_elther.json", encoding="utf8") as file:
-    # #     all_categories = json.load(file)
-    # #
-    # # for item_text ,item_href_1 in all_categories.items():
-    # #     if len(item_text) > 20:
-    # #         url = f'{item_href_1}'
-    # url = 'https://leroymerlin.ru/catalogue/radiatory-otopleniya/'
-    # html = get_html(url, useragent, proxy)
-    # pages_count = get_pages_count(html)
-    # radiators = []
-    # for page in range(1, int(pages_count) + 1):
-    #     print(pages_count)
-    #     print(f'Парсинг страницы {page} из {pages_count}...')
-    #     html = get_html(url, params={'page': page})
-    #     radiators.extend(get_ip(html))
-    #
-    # useragents = open('useragents.txt').read().split('\n')
-    # proxies = open('proxies.txt').read().split('\n')
-    # # Создаем видимость что парсит человек и рандомизируем прокси и юзер агент
-    # # for i in range(2):
-    # #     sleep(uniform(3, 12))
-    # #     proxy = {'http': 'http://' + choice(proxies)}
-    # #     useragent = {'User-Agent': choice(useragents)}
-    #     # try:
-    #         # html = get_html(url, useragent, proxy)
-    #         # pages_count = get_pages_count(html)
-    #         # radiators = []
-    #         # for page in range(1, int(pages_count) + 1):
-    #         #     print(f'Парсинг страницы {page} из {pages_count}...')
-    #         #     html = get_html(url, params={'page': page})
-    #         #     radiators.extend(get_ip(html.text))
-    # save_file(radiators, FILE)
-    # print(f'Получено {len(radiators)} элементов')
-    # # get_ip(html)
-    # os.startfile(FILE)
-    #         #
-    #         # for page in range(1, pages_count + 1):
-            #     print(f'Парсинг страницы {page} из {pages_count}...')
-            #     html = get_html(url, params={'page': page})
-            #     radiators.extend(get_ip(html))
-            # save_file(radiators, FILE)
-            # print(f'Получено {len(radiators)} автомобилей')
-            # else:
-            #     print('Error')
-        # except:
-        #     continue
-        # try:
-        #     # if html.status_code == 200:
-        #     # radiators = []
-        #     # for page in range(1, int(pages_count) + 1):
-        #     #     print(f'Парсинг страницы {page} из {pages_count}...')
-        #     #     html = get_html(url, params={'page': page})
-        #     #     radiators.extend(get_ip(html.text))
-        #     # save_file(radiators, FILE)
-        #     # print(f'Получено {len(radiators)} элементов')
-        #     # # get_ip(html)
-        #     # os.startfile(FILE)
-        #     # else:
-        #     #     print('Erorr')
-        # except AttributeError:
-        #     continue
-
     parse()
+
+
+
 
 if __name__ == '__main__':
     main()
