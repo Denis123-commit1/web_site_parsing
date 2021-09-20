a1b94566d725c6f57580fc335014bd3978c9a37b
Denis123-commit1 <financialstudent1@gmail.com>
2021-09-20 16:17:25 +0300

    Остались дубликаты, надо доработать список

--- parser.py
+++ parser.py
@@ -127,20 +127,61 @@ def associated_list(html):
         #         json.dump(catalog_items_1, file_1, indent=4, ensure_ascii=False)
         #         print('#'*20)
 
-        with open('catalog_items_1.json') as f:
-            data = json.loads("[" +
-                              f.read().replace("\n}{", "\n},{") +
-                              "]")
-        # print(data)
-        result = {}
-        for d in data:
-            result.update(d)
-        # print(result)
-        valuesList = list(result.values())
-        # print(valuesList)
-        with open(f"catalog_items_1_1.json.json", "a", encoding="utf-8") as file:
-            json.dump(valuesList, file, indent=4, ensure_ascii=False)
-        print('-' * 20)
+        # with open('catalog_items_1.json') as f:
+        #     data = json.loads("[" +
+        #                       f.read().replace("\n}{", "\n},{") +
+        #                       "]")
+        # # print(data)
+        # result = {}
+        # for d in data:
+        #     result.update(d)
+        # # print(result)
+        # valuesList = list(result.values())
+        # # print(valuesList)
+        # with open(f"catalog_items_1_1.json", "a", encoding="utf-8") as file:
+        #     json.dump(valuesList, file, indent=4, ensure_ascii=False)
+        # print('-' * 20)
+
+    with open("catalog_items_1_1.json", encoding="utf8") as file:
+        catalog_items_1_1 = json.load(file)
+    for k_1, url_2 in enumerate(catalog_items_1_1):
+        useragents = open('useragents.txt').read().split('\n')
+        proxies = open('proxies.txt').read().split('\n')
+        sleep(uniform(3, 12))
+        proxy = {'http': 'http://' + choice(proxies)}
+        useragent = {'User-Agent': choice(useragents)}
+        req_1 = get_html(url_2, useragent, proxy)
+        print('Next_parser_2')
+        soup_2 = BeautifulSoup(req_1, 'lxml')
+        info_block_1 = soup_2.find_all("a", {"class":"bex6mjh_plp", "data-qa":"catalog-link"})
+        catalog_items_1_1_1 = {}
+        for i_1,item_2 in enumerate(info_block_1):
+            item_link_1 = 'https://leroymerlin.ru' + item_2.get('href')
+            catalog_items_1_1_1[f"{k_1, i_1}"] = item_link_1
+            if k_1 == len(catalog_items_1_1):
+                break
+        with open("catalog_items_1_1_1.json", "a", encoding="utf-8") as file_1:
+            json.dump(catalog_items_1_1_1, file_1, indent=4, ensure_ascii=False)
+            print('&'*20)
+
+    # with open('catalog_items_1.json') as f:
+    #     data = json.loads("[" +
+    #                       f.read().replace("\n}{", "\n},{") +
+    #                       "]")
+    # # print(data)
+    # result = {}
+    # for d in data:
+    #     result.update(d)
+    # # print(result)
+    # valuesList = list(result.values())
+    # # print(valuesList)
+    # with open(f"catalog_items_1_1.json", "a", encoding="utf-8") as file:
+    #     json.dump(valuesList, file, indent=4, ensure_ascii=False)
+    # print('-' * 20)
+
+
+
+
 
 
 
